9304ae20a851a4ef4b068bdea313a86246e58b2f
Andres Soria <andres.soria@hftm.ch>
2024-10-29 00:22:17 +0100

    refactor: app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    DemoComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-blog-app-andres-soria';
+  title = 'Angular Material Blog-App';
 }
