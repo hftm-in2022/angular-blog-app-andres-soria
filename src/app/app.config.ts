711199800a88cab03c78019401c90a3a07f75b0b
Andres Soria <andres.soria@hftm.ch>
2024-11-01 00:20:35 +0100

    feat: implemented new services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { BlogApiService } from './core/services/blog-api.service';
+import { AppErrorHandler } from './core/services/error-handler.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
+    BlogApiService,
+    AppErrorHandler,
   ],
 };
